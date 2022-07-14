578055691a912c37fdf2eb75f237ab7018ec6f47
kamal <mnkskamal@gmail.com>
2022-07-14 10:35:08 -0500

    Minor changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey This is the App from AWS Amplify.
         </p>
         <a
           className="App-link"
