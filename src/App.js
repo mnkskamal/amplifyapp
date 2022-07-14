4105478a44e9a798981156f8f6fa3c0734501597
kamal <mnkskamal@gmail.com>
2022-07-14 11:33:39 -0500

    Added Auth and Backend Amplify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css'
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({signOut}) {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +27,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
